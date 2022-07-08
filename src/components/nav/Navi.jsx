8582c19615967c4d77fdf92db650898ad8e38dc8
Atharva Pandhare <80245432+Atharva-17@users.noreply.github.com>
2022-07-08 11:43:05 +0530

    icons updated

--- src/components/nav/Navi.jsx
+++ src/components/nav/Navi.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './nav.css';
-import {AiOutlineHome} from 'react-icons/ai';
-import {AiOutlineUser} from 'react-icons/ai';
-import {BiBook} from 'react-icons/bi';
-import {RiServiceLine} from 'react-icons/ri';
-import {BiMessageSquareDetail} from 'react-icons/bi';
+import {SiHomeassistant} from 'react-icons/si';
+import {FaUserAstronaut} from 'react-icons/fa';
+import {GiTechnoHeart} from 'react-icons/gi';
+import {GoProject} from 'react-icons/go';
+import {MdContactMail} from 'react-icons/md';
 import {useState} from 'react';
 
 function Navi() {
@@ -12,11 +12,11 @@ function Navi() {
   return (
      <>
      <nav>
-        <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav==='#home' ? 'active' : ''}><AiOutlineHome/><div className='tooltip'>Home</div></a>
-        <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav==='#about' ? 'active' : ''}><AiOutlineUser/><div className='tooltip'>About</div></a>
-        <a href="#tech" onClick={()=> setActiveNav('#tech')} className={activeNav==='#tech' ? 'active' : ''}><BiBook/><div className='tooltip'>Tech</div></a>
-        <a href="#projects" onClick={()=> setActiveNav('#projects')} className={activeNav==='#projects' ? 'active' : ''}><RiServiceLine/><div className='tooltip'>Projects</div></a>
-        <a href="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact' ? 'active' : ''}><BiMessageSquareDetail/><div className='tooltip'>Contact</div></a>
+        <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav==='#home' ? 'active' : ''}><SiHomeassistant/><div className='tooltip'>Home</div></a>
+        <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav==='#about' ? 'active' : ''}><FaUserAstronaut/><div className='tooltip'>About</div></a>
+        <a href="#tech" onClick={()=> setActiveNav('#tech')} className={activeNav==='#tech' ? 'active' : ''}><GiTechnoHeart/><div className='tooltip'>Tech</div></a>
+        <a href="#projects" onClick={()=> setActiveNav('#projects')} className={activeNav==='#projects' ? 'active' : ''}><GoProject/><div className='tooltip'>Projects</div></a>
+        <a href="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact' ? 'active' : ''}><MdContactMail/><div className='tooltip'>Contact</div></a>
      </nav>
      </>
   )

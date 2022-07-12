3ce5374c23c04e8926682eb0a22fc4e7a228ed06
Atharva Pandhare <80245432+Atharva-17@users.noreply.github.com>
2022-07-12 21:41:37 +0530

    minor changes

--- src/components/education/Edu.jsx
+++ src/components/education/Edu.jsx
@@ -12,7 +12,9 @@ function Edu() {
             online stores.</p>
     <div className='main__edu'>
    <div class="work" data-aos="fade-up">
-      <h3><i class="fa fa-graduation-cap " id='briefcase'></i>Education</h3>
+      <h3>
+        {/* <i class="fa fa-graduation-cap " id='briefcase'></i> */}
+        Education</h3>
       <ul>
         <li><span>Bachelor’s of Engineering – CSE</span><small>Shri Ramdeobaba College of Engineering, Nagpur</small><small>2019 - 2023</small></li>
         <li><span>Class 12</span><small>Santaji Mahavidyalaya, Nagpur</small><small>2017 - 2019</small></li>
@@ -20,7 +22,9 @@ function Edu() {
       </ul>
     </div>
     <div class="edu">
-      <h3><i class="fa fa-briefcase" id='cap'></i>Experience</h3>
+      <h3>
+        {/* <i class="fa fa-briefcase" id='cap'></i> */}
+        Experience</h3>
       <ul>
         <li><span>Web Developer Intern</span><small>UFF Food Pvt. Ltd.</small><small>June 2021 - August 2021</small></li>
         <li><span>Web Developer</span><small>Google Developer Student Club(GDSC)</small><small>August 2021 - June 2022</small></li>

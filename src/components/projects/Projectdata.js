df96cca83fb54880d96face117657f63be2176be
Atharva Pandhare <80245432+Atharva-17@users.noreply.github.com>
2022-07-12 21:38:12 +0530

    projects updated

--- src/components/projects/Projectdata.js
+++ src/components/projects/Projectdata.js
@@ -1,21 +1,76 @@
 export const ProjectData =[
     {
         id:1,
+        title:"Quiz App",
+        about:"Build a React Quiz app using Firebase and deployed on Heroku. Implemented functionalities like Email Login, realtime database using Firebase, Fetch API, Notifications Channels.",
+        tags:['React','HTML','CSS','Javascript','NodeJs','Firebase','Heroku'],
+        demo:"https://react-quiz-app17.herokuapp.com/",
+        github:"https://github.com/Atharva-17/Portfolio-Website",
+        image:"https://xp.io/storage/2at0qxWq.jpg"
+    },
+    {
+        id:1,
+        title:"Facial Emotion Recognition System",
+        about:"Problem Statement : Detecting facial expression and checking whether the customer is happy or sad with the services. The objective is to use a deep neural network to identify faces which makes it easier for the system to detect the emotion. For detecting the face, Cvlib Library is used. After detecting the faces, the Deep learning model is used to recognize the emotions. Finally, Analysing the data obtained by the facial emotion recognition system to help the cafe owners to improve their services. The model is trained on the FER-2013 dataset which was published at International Conference on Machine Learning (ICML).",
+        tags:['Flask','HTML','CSS','EJS','OpenCV','Cvlib','Python','Matplot','Numpy'],
+        demo:"https://github.com/Atharva-17/Facial-Emotion-Detection",
+        github:"https://github.com/Atharva-17/Facial-Emotion-Detection",
+        image:"https://xp.io/storage/2auLSmky.png"
+    },
+    {
+        id:1,
         title:"Unibuzz Solutions",
         about:"Unibuzz marketing solution Provides digital marketing services for various businesses. Designed the entire UI and implemented Custom CSS animation, also developed the Dynamic Contact Us Form.",
         tags:['Freelance','HTML','CSS','Javascript','jQuery'],
-        demo:"ebfje",
-        github:"vjvjhv",
+        demo:"https://github.com/Atharva-17/Facial-Emotion-Recognition",
+        github:"https://github.com/Atharva-17/unibuzz_solution",
         image:"https://xp.io/storage/20V2mmNl.png"
     },
 
     {
         id:1,
-        title:"Project 1",
-        about:"hsgiegufuuououo3wu3oruor33rvou",
-        tags:['Reactjs','NodeJs'],
-        demo:"ebfje",
-        github:"vjvjhv",
-        image:"ksbhbbcb"
+        title:"H. Thakare",
+        about:"H.Thakare is a real estate consultant. The client’s requirement was to make a portfolio landing page. For this project, I designed fully Responsive UI and developed Custom CSS animations, Deployed the site on given servers.",
+        tags:['Freelance','HTML','CSS','Javascript'],
+        demo:"https://www.hthakare.in/",
+        github:"https://github.com/Atharva-17/harshal_thakare",
+        image:"https://xp.io/storage/2atAwOqb.png"
+    },
+
+    {
+        id:1,
+        title:"Decentradata",
+        about:"Decentradata(Appraising Decentraland’s virtual real estate using machine learning to predict investments that will yield the best). Participated in hackdartmouth hackathon and developed the Frontend part of this project. It was a really fun experience for me while developing decentradata and learning new tech.",
+        tags:['Hackathon','HTML','CSS','Javascript','React','Python'],
+        demo:"https://www.youtube.com/watch?v=rDaIEwoN8-8",
+        github:"https://github.com/seanroades/decentradata",
+        image:"https://xp.io/storage/2avGEWQ2.png"
+    },
+    {
+        id:1,
+        title:"Domino's Pizza 2.0",
+        about:"This was my technical round project for UFF(internship). They said that you have create a single webpage with a clean design and dummy information introducing any food brand of your choice. So, I made dominos 2.0. It was an amazing experience while developing this project, learned a lot of new techs!",
+        tags:['HTML','CSS','SCSS','Javascript'],
+        demo:"https://atharva-17.github.io/dominos-pizza-2.0.github.io/",
+        github:"https://github.com/Atharva-17/dominos-pizza-2.0.github.io",
+        image:"https://xp.io/storage/2aw6jREv.png"
+    },
+    {
+        id:1,
+        title:"Teasure Hunt Landing Page",
+        about:"GDSC organizes Online Treasure Hunt for the 1st and 2nd-year Students.The theme was based on Avengers. We developed the Treasure Hunt Website which is consisted of Trivia Questions related to Avengers. I manage the Web Team and build the UI and CSS animations. I also contributed on the backend side and manages to develop dynamic displaying of questions using Django. Finally, Deploy the site on Netlify.",
+        tags:['GDSC','HTML','CSS','SCSS','Javascript'],
+        demo:"https://rcoem-google-dsc.github.io/Teasure-hunt-Landing-Page/",
+        github:"https://github.com/RCOEM-Google-DSC/Teasure-hunt-Landing-Page",
+        image:"https://xp.io/storage/2awHU0w8.png"
+    },
+    {
+        id:1,
+        title:"GDSC Website",
+        about:"Basically, I created a custom CSS animation for the website and manages the Front-end Web team. Worked with the Team and Manages to build various Events, Contest, Workshops, GDSC Website.",
+        tags:['GDSC','HTML','CSS','SCSS','Javascript'],
+        demo:"https://rcoem-gdsc.netlify.app/",
+        github:"https://github.com/RCOEM-Google-DSC",
+        image:"https://xp.io/storage/2ax8t1qq.png"
     },
 ]
